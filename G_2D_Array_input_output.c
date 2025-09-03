d7b3386437dfc36aaa3acb1cde69e788413cd9ce
Md Fazla Rabbi Abir <131839058+fazla-rabbi-abir@users.noreply.github.com>
2025-09-03 19:16:03 +0600

    Add files via upload

---
+++ G_2D_Array_input_output.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+    int row, col;
+    printf("Enter row and column : ");
+    scanf("%d %d", &row, &col);
+
+    int arr[row][col];
+
+    for(int i=0; i<row; i++)
+    {
+        for(int j=0; j<col; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    for(int i=0; i<row; i++)
+    {
+        for(int j=0; j<col; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
